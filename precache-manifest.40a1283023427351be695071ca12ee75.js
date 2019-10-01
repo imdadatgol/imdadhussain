fe3ced3cf8d02acd8d7c1301cea8c3fa4982c57a
Imdad <imdad.hussain@goldevelopers.com>
2019-10-01 10:25:16 +0500

    Updates

---
+++ precache-manifest.40a1283023427351be695071ca12ee75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72680e6b4c01f93465d4c5093537cd04",
+    "url": "/imdadhussain/index.html"
+  },
+  {
+    "revision": "bee0e95dd4c77d0ea525",
+    "url": "/imdadhussain/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b66b8fc6a4310c55fef2",
+    "url": "/imdadhussain/static/js/2.1d6e97e5.chunk.js"
+  },
+  {
+    "revision": "bee0e95dd4c77d0ea525",
+    "url": "/imdadhussain/static/js/main.7ca44114.chunk.js"
+  },
+  {
+    "revision": "48ed7aba3265166ea3bc",
+    "url": "/imdadhussain/static/js/runtime~main.19e74388.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/imdadhussain/static/media/logo.5d5d9eef.svg"
+  }
+]);
